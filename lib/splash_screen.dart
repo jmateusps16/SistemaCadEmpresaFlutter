c2a0f9ff0f2d19e12452c655cdc971b813e3d72b
Mateus Palmeira <jmateusps16@gmail.com>
2022-10-17 01:59:03 -0300

    Tela Splash Screen Finalizada

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -5,7 +5,27 @@ class SplashScreen extends StatefulWidget {
   State<StatefulWidget> createState() => InitState();
 }
 
-class InitState extends State<SplashScreen> {
+class InitState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return InitWidget();
@@ -20,10 +40,23 @@ class InitState extends State<SplashScreen> {
               decoration: const BoxDecoration(
                 color: Color.fromARGB(255, 243, 241, 240),
               ),
-            ),
-            Center(
-              child: Container(
-                child: Image.asset('images/aimvx_logo.png'),
+              child: SizedBox(
+                child: Center(
+                  child: SizedBox(
+                    height: 170,
+                    width: 250,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image.asset('images/aimvx_logo.png'),
+                        LinearProgressIndicator(
+                          value: controller.value,
+                        ),
+                        const Text('Carregando...')
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
