92971d67c4ecd6449578b5e8338c2ff507a31477
Mateus Palmeira <jmateusps16@gmail.com>
2022-10-19 23:49:48 -0300

    Adição de Botões e Checkbox

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,16 +1,96 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _LoginScreenState extends State<LoginScreen> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+          body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 80),
+              height: 200,
+              child: Center(
+                child: SizedBox(
+                  height: 170,
+                  width: 250,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(children: <Widget>[
+                        Image.asset('images/aimvx_logo.png'),
+                        const Text("Sistema de Cadastro de Empresa",
+                            style: TextStyle(fontSize: 16))
+                      ])
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const Center(
+                child: SizedBox(
+                    width: 280,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Usuário ou e-mail',
+                      ),
+                    ))),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 51),
+                  child: SizedBox(
+                    child: Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: const SizedBox(
+                    width: 280,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ))),
+            ElevatedButton(
+              onPressed: () {
+                // vai para Quero fazer parte!
+              },
+              child: const Text('Entrar'),
+            ),
+            Center(
+              child: OutlinedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Voltar'),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
